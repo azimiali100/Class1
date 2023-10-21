95965d70e9570b5406dbf959f932f27812d02ee2
AliHasaniAzimi <147371337+azimiali100@users.noreply.github.com>
2023-10-21 18:37:28 +0330

    Add files via upload

---
+++ تمرین2.1.py
@@ -0,0 +1,8 @@
+#دریافت ورودی از کاربر
+first_name = input("نام: ")
+last_name = input("نام خانوادگی: ")
+age = input("سن: ")
+#چاپ اطلاعات
+print("نام: " + first_name)
+print("نام خانوادگی: " + last_name)
+print("سن: " + age)
---
+++ تمرین2.2.py
@@ -0,0 +1,6 @@
+# برنامه‌ای که Syntax Error دارد
+print("Hello, World!"
+# برنامه‌ای که Semantic Error دارد
+x = 5
+y = "2"
+z = x + y
